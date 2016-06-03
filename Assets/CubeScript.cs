d5c0a77adbe5cf57318ce74b7909270476c2d6cf
Maz Jaleel <mazjaleel@gmail.com>
2016-06-03 16:23:15 +0300

    initial commit

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+	#region Animation Callbacks
+
+	private bool goRight = false;
+
+	public void PieceAnimationOnJump() {
+
+		var location = goRight ? 0.0f : 3.0f;
+		LeanTween.moveLocalX(gameObject, location, 0.6f);
+		LeanTween.moveLocalY(gameObject, 4.0f, 0.3f)
+			.setEase(LeanTweenType.easeOutCubic)
+			.setOnComplete(() => 
+				LeanTween.moveLocalY(gameObject, 0.5f, 0.3f)
+				.setEase(LeanTweenType.easeInCubic)
+			);
+
+		goRight = !goRight;
+	}
+
+	#endregion
+}
