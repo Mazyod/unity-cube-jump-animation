7a21a5c4c70a6b45c70c2c674d3c53e022662e6c
Maz Jaleel <mazjaleel@gmail.com>
2016-07-04 00:23:05 +0300

    added death and spwn animations

--- Assets/CubeScript.cs
+++ Assets/CubeScript.cs
@@ -3,13 +3,74 @@ using System.Collections;
 
 public class CubeScript : MonoBehaviour {
 
-	#region Animation Callbacks
+	public enum Animation {
+		Jump,
+		Swpn,
+		Death,
+		Promotion,
+	}
 
+	public Animation animationType;
+
+	private Vector3 initialPosition;
+	private Animator animator;
 	private bool goRight = false;
+
+
+	#region init & dealloc
+
+	void Start() {
+
+		animator = GetComponent<Animator>();
+		initialPosition = transform.localPosition;
+
+		switch (animationType) {
+		case Animation.Jump:
+			PerformJumpAnimation();
+			break;
+
+		case Animation.Swpn:
+			PerformSpwnAnimation();
+			break;
+
+		case Animation.Death:
+			PerformDeathAnimation();
+			break;
+
+		case Animation.Promotion:
+			PerformPromotionAnimation();
+			break;
+		}
+	}
+
+	#endregion
+
+	#region private methods
+
+	private void PerformJumpAnimation() {
+		animator.SetTrigger("jump");
+	}
+
+	private void PerformSpwnAnimation() {
+
+	}
+
+	private void PerformDeathAnimation() {
+		animator.SetTrigger("die");
+	}
+
+	private void PerformPromotionAnimation() {
+
+	}
+
+	#endregion
+
+	#region Animation Callbacks
 
 	public void PieceAnimationOnJump() {
 
-		var location = goRight ? 0.0f : 3.0f;
+		var location = initialPosition.x + (goRight ? 0.0f : 3.0f);
+
 		LeanTween.moveLocalX(gameObject, location, 0.6f);
 		LeanTween.moveLocalY(gameObject, 4.0f, 0.3f)
 			.setEase(LeanTweenType.easeOutCubic)
@@ -19,6 +80,25 @@ public class CubeScript : MonoBehaviour {
 			);
 
 		goRight = !goRight;
+	}
+
+	public void PieceAnimationOnDeathStart() {
+
+		var location = initialPosition.y + 2f;
+		LeanTween.moveLocalY(gameObject, location, 0.38f)
+			.setEase(LeanTweenType.easeInOutExpo);
+	}
+
+	public void PieceAnimationOnDeathEnd() {
+	}
+
+	public void PieceAnimationOnSpwnDidScale() {
+		LeanTween.moveLocalY(gameObject, initialPosition.y, 0.25f)
+			.setEase(LeanTweenType.easeInCubic);
+	}
+
+	public void PieceAnimationOnSpwnEnd() {
+
 	}
 
 	#endregion
